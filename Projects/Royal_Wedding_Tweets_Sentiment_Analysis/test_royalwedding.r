3fdd8b4c8d51b968bc223c88aa9842cb11739dab
Arghyadeep Sen <50789699+ArghyaDS@users.noreply.github.com>
2019-05-28 22:36:20 +0530

    Projects updated

---
+++ Projects/Royal_Wedding_Tweets_Sentiment_Analysis/test_royalwedding.r
@@ -0,0 +1,89 @@
+# Load the required Packages
+library(twitteR)
+library(RCurl)
+library(httr)
+library(tm)
+library(wordcloud)
+library(syuzhet)
+
+# authorisation keys
+consumer_key = "CHfCGOUA4n79rPfUv51ho3KuZ" 
+consumer_secret = "oIaYcqSO4cPIdTI6AQgNnEPF43liHDmT8McWhzuWb5s07yLRW6" 
+access_token = "3301214961-dyFRicbKPGZnBdObGB2C0327GhhQGUsA3Gpc1x3" 
+access_secret ="pA3YcHPcHfk2wpyCtczCQQnfY37TzmGc2K3mUJ2zjpTtD" 
+
+# set up
+setup_twitter_oauth(consumer_key,consumer_secret,access_token, access_secret)
+
+## [1] "Using direct authentication"
+
+# search for tweets in english language
+tweets = searchTwitter("#RoyalWedding", n = 10000, lang = "en")
+
+# store the tweets into dataframe
+tweets.df = twListToDF(tweets)
+
+# CLEANING TWEETS
+
+tweets.df$text=gsub("&amp", "", tweets.df$text)
+tweets.df$text = gsub("&amp", "", tweets.df$text)
+tweets.df$text = gsub("(RT|via)((?:\\b\\W*@\\w+)+)", "", tweets.df$text)
+tweets.df$text = gsub("@\\w+", "", tweets.df$text)
+tweets.df$text = gsub("[[:punct:]]", "", tweets.df$text)
+tweets.df$text = gsub("[[:digit:]]", "", tweets.df$text)
+tweets.df$text = gsub("http\\w+", "", tweets.df$text)
+tweets.df$text = gsub("[ \t]{2,}", "", tweets.df$text)
+tweets.df$text = gsub("^\\s+|\\s+$", "", tweets.df$text)
+
+tweets.df$text <- iconv(tweets.df$text, "UTF-8", "ASCII", sub="")
+
+# Emotions for each tweet using NRC dictionary
+emotions <- get_nrc_sentiment(tweets.df$text)
+emo_bar = colSums(emotions)
+emo_sum = data.frame(count=emo_bar, emotion=names(emo_bar))
+emo_sum$emotion = factor(emo_sum$emotion, levels=emo_sum$emotion[order(emo_sum$count, decreasing = TRUE)])
+
+# Visualize the emotions from NRC sentiments
+library(plotly)
+p <- plot_ly(emo_sum, x=~emotion, y=~count, type="bar", color=~emotion) %>%
+  layout(xaxis=list(title=""), showlegend=FALSE,
+         title="Emotion Type for hashtag: #RoyalWedding")
+api_create(p,filename="Sentimentanalysis")
+
+# Create comparison word cloud data
+
+wordcloud_tweet = c(
+  paste(tweets.df$text[emotions$anger > 0], collapse=" "),
+  paste(tweets.df$text[emotions$anticipation > 0], collapse=" "),
+  paste(tweets.df$text[emotions$disgust > 0], collapse=" "),
+  paste(tweets.df$text[emotions$fear > 0], collapse=" "),
+  paste(tweets.df$text[emotions$joy > 0], collapse=" "),
+  paste(tweets.df$text[emotions$sadness > 0], collapse=" "),
+  paste(tweets.df$text[emotions$surprise > 0], collapse=" "),
+  paste(tweets.df$text[emotions$trust > 0], collapse=" ")
+)
+
+# create corpus
+corpus = Corpus(VectorSource(wordcloud_tweet))
+
+# remove punctuation, convert every word in lower case and remove stop words
+
+corpus = tm_map(corpus, tolower)
+corpus = tm_map(corpus, removePunctuation)
+corpus = tm_map(corpus, removeWords, c(stopwords("english")))
+corpus = tm_map(corpus, stemDocument)
+
+# create document term matrix
+
+tdm = TermDocumentMatrix(corpus)
+
+# convert as matrix
+tdm = as.matrix(tdm)
+tdmnew <- tdm[nchar(rownames(tdm)) < 11,]
+
+# column name binding
+colnames(tdm) = c('anger', 'anticipation', 'disgust', 'fear', 'joy', 'sadness', 'surprise', 'trust')
+colnames(tdmnew) <- colnames(tdm)
+comparison.cloud(tdmnew, random.order=FALSE,
+                 colors = c("#00B2FF", "red", "#FF0099", "#6600CC", "green", "orange", "blue", "brown"),
+                 title.size=1, max.words=250, scale=c(2.5, 0.4),rot.per=0.4)
---
+++ Projects/Top_Tweets_Sentiment_Analysis/test_project.R
@@ -0,0 +1,38 @@
+# Load Requried Packages
+library("SnowballC")
+library("tm")
+library("twitteR")
+library("syuzhet")
+# Authonitical keys
+consumer_key <- "CHfCGOUA4n79rPfUv51ho3KuZ" 
+consumer_secret <- "oIaYcqSO4cPIdTI6AQgNnEPF43liHDmT8McWhzuWb5s07yLRW6" 
+access_token <- "3301214961-dyFRicbKPGZnBdObGB2C0327GhhQGUsA3Gpc1x3" 
+access_secret <- "pA3YcHPcHfk2wpyCtczCQQnfY37TzmGc2K3mUJ2zjpTtD"
+setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
+tweets <- userTimeline("CaptainMarvel", n=200)
+n.tweet <- length(tweets)
+tweets.df <- twListToDF(tweets) 
+head(tweets.df)
+head(tweets.df$text)
+tweets.df2 <- gsub("http.*","",tweets.df$text)
+tweets.df2 <- gsub("https.*","",tweets.df2)
+tweets.df2 <- gsub("#.*","",tweets.df2)
+tweets.df2 <- gsub("@.*","",tweets.df2)
+head(tweets.df2)
+word.df <- as.vector(tweets.df2)
+emotion.df <- get_nrc_sentiment(word.df)
+emotion.df2 <- cbind(tweets.df2, emotion.df) 
+head(emotion.df2)
+sent.value <- get_sentiment(word.df)
+most.positive <- word.df[sent.value == max(sent.value)]
+most.positive
+most.negative <- word.df[sent.value <= min(sent.value)] 
+most.negative
+sent.value
+positive.tweets <- word.df[sent.value > 0]
+head(positive.tweets)
+negative.tweets <- word.df[sent.value < 0]
+head(negative.tweets)
+neutral.tweets <- word.df[sent.value == 0]
+head(neutral.tweets)
+
